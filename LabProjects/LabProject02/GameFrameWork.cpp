5d5ac67556abe18ce36df00ccc1d4b5f38465169
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2024-03-07 21:59:27 +0900

    따라하기(03)_4. CreateDirect3DDisplay() 함수 정의
    4. CreateDirect3DDisplay() 함수를 정의함.

--- LabProjects/LabProject02/GameFrameWork.cpp
+++ LabProjects/LabProject02/GameFrameWork.cpp
@@ -143,3 +143,78 @@ void CGameFramework::CreateSwapChain()
 	m_nSwapChainBufferIndex = m_pdxgiSwapChain->GetCurrentBackBufferIndex();
 	// 스왑체인의 현재 후면버퍼 인덱스를 저장한다.
 }
+
+void CGameFramework::CreateDirect3DDevice()
+{
+	HRESULT hResult;
+
+	UINT nDXGIFactoryFlags = 0;
+#if defined(_DEBUG)
+	ID3D12Debug* pd3dDebugController = NULL;
+	hResult = D3D12GetDebugInterface(__uuidof(ID3D12Debug), (void**)&pd3dDebugController);
+	if (pd3dDebugController)
+	{
+		pd3dDebugController->EnableDebugLayer();
+		pd3dDebugController->Release();
+	}
+	nDXGIFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+	hResult = ::CreateDXGIFactory2(nDXGIFactoryFlags, __uuidof(IDXGIFactory4), (void**)&m_pdxgiFactory);
+	
+	IDXGIAdapter1* pd3dAdapter = NULL;
+	for (UINT i = 0; DXGI_ERROR_NOT_FOUND != m_pdxgiFactory->EnumAdapters1(i, &pd3dAdapter); i++)
+	{
+		DXGI_ADAPTER_DESC1 dxgiAdapterDesc;
+		pd3dAdapter->GetDesc1(&dxgiAdapterDesc);
+		if (dxgiAdapterDesc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+			continue;
+		if (SUCCEEDED(D3D12CreateDevice(pd3dAdapter, D3D_FEATURE_LEVEL_12_0,
+			__uuidof(ID3D12Device), (void**)&m_pd3dDevice)))
+			break;
+	}
+	// 모든 하드웨어 어댑터에 대하여 특성 레벨 12.0을 지원하는 하드웨어 디바이스를 생성한다.
+
+	if (!pd3dAdapter)
+	{
+		m_pdxgiFactory->EnumWarpAdapter(__uuidof(IDXGIAdapter1), (void**)&pd3dAdapter);
+		D3D12CreateDevice(pd3dAdapter, D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device),
+			(void**)&m_pd3dDevice);
+	}
+	// 특성 레벨 12.0을 지원하는 하드웨어 디바이스를 생성할 수 없으면 WARP 디바이스를 생성한다.
+
+	D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS d3dMsaaQualityLevels;
+	d3dMsaaQualityLevels.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	d3dMsaaQualityLevels.SampleCount = 4;	// Msaa4x 다중 샘플링
+	d3dMsaaQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+	d3dMsaaQualityLevels.NumQualityLevels = 0;
+	m_pd3dDevice->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS,
+		&d3dMsaaQualityLevels, sizeof(D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS));
+	m_uMsaa4xQualityLevels = d3dMsaaQualityLevels.NumQualityLevels;
+	// 디바이스가 지원하는 다중 샘플의 품질 수준을 확인하다.
+	m_bMsaa4xEnable = (m_uMsaa4xQualityLevels > 1) ? true : false;
+	// 다중 샘플의 품질 수준이 1보다 크면 다중 샘플링을 활성화한다.
+
+	hResult = m_pd3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, __uuidof(ID3D12Fence),
+		(void**)&m_pd3dFence);
+	m_nFenceValue = 0;
+	// 펜스를 생성하고 펜스 값을 0으로 설정한다.
+
+	m_hFenceEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+	// 펜스와 동기화를 위한 이벤트 객체를 생성한다(이벤트 객체의 초기값은 FALSE이다).
+	// 이벤트가 실행되면(Signal) 이벤트의 값을 자동적으로 FALSE가 되도록 생성한다.
+
+	m_d3dViewport.TopLeftX = 0;
+	m_d3dViewport.TopLeftY = 0;
+	m_d3dViewport.Width = static_cast<float>(m_nWndClientWidth);
+	m_d3dViewport.Height = static_cast<float>(m_nWndClientHeight);
+	m_d3dViewport.MinDepth = 0.0f;
+	m_d3dViewport.MaxDepth = 1.0f;
+	// 뷰포트를 주 윈도우의 클라이언트 영역 전체로 설정한다.
+
+	m_d3dScissorRect = { 0,0,m_nWndClientWidth, m_nWndClientHeight };
+	// 씨저 사각형을 주 윈도우의 클라이언트 영역 전체로 설정한다.
+	
+	if (pd3dAdapter)
+		pd3dAdapter->Release();
+}
