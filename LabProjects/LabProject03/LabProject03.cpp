98a15ffeb7b614c2b257c07dbae85bc167af7e05
YoonLeeHana(Home) <lhnyoon@tukorea.ac.kr>
2024-03-16 18:29:53 +0900

    LP02_1, 03.cpp의 InitInstance 함수 수정

--- LabProjects/LabProject03/LabProject03.cpp
+++ LabProjects/LabProject03/LabProject03.cpp
@@ -120,11 +120,11 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
     RECT rc = { 0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT };
     DWORD dwStyle = WS_OVERLAPPED | WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU | WS_BORDER;
     AdjustWindowRect(&rc, dwStyle, FALSE);
-
-    //hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
     HWND hMainWnd = CreateWindow(szWindowClass, szTitle, dwStyle, CW_USEDEFAULT,
         CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
